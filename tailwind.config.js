7597232cbccc4f4b5af3b5f1fe54ab3396e621a7
Dylan K <50585244+dylankainth@users.noreply.github.com>
2025-02-08 12:03:48 +0000

    added flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,13 @@ export default {
     "./pages/**/*.vue",
     "./plugins/**/*.{js,ts}",
     "./nuxt.config.{js,ts}",
+    "./node_modules/flowbite/**/*.{js,ts}"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
