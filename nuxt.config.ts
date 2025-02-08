094326f8292d9b76d9f1794dc5ad8fddc79f24dc
Dylan K <50585244+dylankainth@users.noreply.github.com>
2025-02-08 12:27:59 +0000

    added pwa support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,34 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  ssr: false,
   modules: [
-    '@nuxtjs/tailwindcss'
-  ]
+    '@nuxtjs/tailwindcss',
+    '@vite-pwa/nuxt',
+  ],
+  pwa: {
+    manifest: {
+      name: 'WalkieTalkie',
+      short_name: 'WalkieTalkie',
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#ffffff',
+      theme_color: '#ffffff',
+      description: 'Your AI Tour Guide',
+      lang: 'en',
+      icons: [
+        // {
+        //   src: '/logo.svg',
+        //   sizes: '192x192',
+        //   type: 'image/svg',
+        // },
+        // {
+        //   src: '/logo512.png',
+        //   sizes: '512x512',
+        //   type: 'image/png',
+        // },
+      ]
+    }
+  },
+
 })
